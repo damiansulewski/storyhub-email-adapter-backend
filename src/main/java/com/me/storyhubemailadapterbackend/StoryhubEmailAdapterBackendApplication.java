16a8d216703271a22d0f68bc580134f352b22a9c
Damian Sulewski <damiansulewskiwork@gmail.com>
2019-12-30 22:16:04 +0100

    Init

---
+++ src/main/java/com/me/storyhubemailadapterbackend/StoryhubEmailAdapterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.me.storyhubemailadapterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryhubEmailAdapterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryhubEmailAdapterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/storyhubemailadapterbackend/StoryhubEmailAdapterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.storyhubemailadapterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryhubEmailAdapterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
